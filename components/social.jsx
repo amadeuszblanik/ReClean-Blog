7d01cfe92560ce21ddad9d400b96c27816ede48b
amadeuszblanik <amadeusz@blanik.me>
2019-05-13 21:27:10 +0200

    Create social navigation

--- components/social.jsx
+++ components/social.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import Link from "next/link";
 import styles from "../styles/components/social.scss";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faFacebook, faTwitter, faInstagram} from '@fortawesome/free-brands-svg-icons'
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
 
 export default class Search extends React.Component {
     constructor(props, req) {
@@ -10,7 +13,17 @@ export default class Search extends React.Component {
     render() {
         return(
             <div className={styles.social}>
-                Social
+                <nav className={styles.navigation}>
+                    <a className={styles.navItem}>
+                    <FontAwesomeIcon icon={faFacebook}/>
+                    </a>
+                    <a className={styles.navItem}>
+                        <FontAwesomeIcon icon={faTwitter}/>
+                    </a>
+                    <a className={styles.navItem}>
+                        <FontAwesomeIcon icon={faInstagram}/>
+                    </a>
+                </nav>
             </div>
         )
     }
