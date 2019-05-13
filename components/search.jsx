87a9b9128bf0c7d33a2b546c2dc35d70602561bc
amadeuszblanik <amadeusz@blanik.me>
2019-05-13 21:26:56 +0200

    Create search bar

--- components/search.jsx
+++ components/search.jsx
@@ -1,6 +1,8 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Link from "next/link";
 import styles from "../styles/components/search.scss";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faSearch} from '@fortawesome/free-solid-svg-icons'
 
 export default class Search extends React.Component {
     constructor(props, req) {
@@ -8,10 +10,15 @@ export default class Search extends React.Component {
     }
 
     render() {
-        return(
+        return (
             <div className={styles.search}>
                 <form className={styles.searchForm}>
-                    Search
+                    <div className={styles.input}>
+                        <input placeholder="What you are looking for…"/>
+                    </div>
+                    <button className={styles.buttonSubmit}>
+                        <FontAwesomeIcon icon={faSearch}/>
+                    </button>
                 </form>
             </div>
         )
